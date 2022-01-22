3c9948e04fffdd88597002df7d28d493f34ead0f
milad parvaneh <parvanehmilad73@yahoo.com>
2022-01-22 19:09:01 +0330

    final edit adding binding and fix errors

--- app/src/main/java/com/example/testshanbeh/MainActivity.kt
+++ app/src/main/java/com/example/testshanbeh/MainActivity.kt
@@ -1,127 +1,108 @@
 package com.example.testshanbeh
-
-import android.annotation.SuppressLint
 import android.content.Context
-
-import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Patterns
 import android.view.View
-
 import android.widget.*
-import androidx.annotation.RequiresApi
-import androidx.appcompat.widget.LinearLayoutCompat
+import com.example.testshanbeh.databinding.ActivityMainBinding
 
 
 class MainActivity : AppCompatActivity() {
-    @SuppressLint("CommitPrefEdits")
-    @RequiresApi(Build.VERSION_CODES.M)
+    private lateinit var mainbinding :ActivityMainBinding
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        val fullname = findViewById<EditText>(R.id.fullname)
-        val username = findViewById<EditText>(R.id.username)
-        val email = findViewById<EditText>(R.id.email)
-        val pass = findViewById<EditText>(R.id.password)
-        val repass = findViewById<EditText>(R.id.repassword)
-        val gender = findViewById<RadioGroup>(R.id.radioGroup1)
-        val radiobutton1 = findViewById<RadioButton>(R.id.radioButton1)
-        val radiobutton2 = findViewById<RadioButton>(R.id.radioButton2)
-        val btnreg = findViewById<Button>(R.id.btnregister)
-        val btninf = findViewById<Button>(R.id.btnshowinfo)
-        val fullnameinfo = findViewById<TextView>(R.id.fullnameinfo)
-        val usernameinfo = findViewById<TextView>(R.id.usernameinfo)
-        val emailinfo = findViewById<TextView>(R.id.emailinfo)
-        val passinfo = findViewById<TextView>(R.id.passinfo)
-        val genderinfo = findViewById<TextView>(R.id.geninfo)
-        val minfo = findViewById<LinearLayoutCompat>(R.id.minfo)
-        minfo.visibility = View.INVISIBLE
+            mainbinding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(mainbinding.root)
+        mainbinding.minfo.visibility = View.INVISIBLE
         val hidinfo = findViewById<Button>(R.id.hideinfo)
-        val shpref = getSharedPreferences("info", Context.MODE_PRIVATE)
+        val sharepreferences = getSharedPreferences("info", Context.MODE_PRIVATE)
         fun validateFullName():Boolean{
-          return  if (fullname.text.isEmpty()) {
-              fullname.error = "field can't be empty!!"
+          return  if (mainbinding.fullname.text.isEmpty()) {
+              mainbinding.fullname.error = "field can't be empty!!"
               false
             }else true
 
         }
         fun validateUserName():Boolean{
-           return if (username.length() == 0) {
-               username.error = "field can't be empty!!"
+           return if (mainbinding.username.length() == 0) {
+               mainbinding.username.error = "field can't be empty!!"
                false
             }else true
         }
         fun validateEmail():Boolean{
-          return    if (email.length() == 0){
-              email.error = "field can't be empty!!"
+          return    if (mainbinding.email.length() == 0){
+              mainbinding.email.error = "field can't be empty!!"
               false
-          }else if (!Patterns.EMAIL_ADDRESS.matcher(email.text.trim()).matches()) {
-              email.error = "unvalid email address!!"
+          }else if (!Patterns.EMAIL_ADDRESS.matcher(mainbinding.email.text.trim()).matches()) {
+              mainbinding.email.error = "unvalid email address!!"
               false
           }else true
         }
         fun validatePassword():Boolean{
-           return if (pass.length() == 0) {
-               pass.error = "field can't be empty!!"
+           return if (mainbinding.password.length() == 0) {
+               mainbinding.password.error = "field can't be empty!!"
                false
            }else true
         }
         fun validateRetypePassword():Boolean{
-           return if(repass.text.isEmpty()){
-               repass.error = "field can't be empty!!"
+           return if(mainbinding.repassword.text.isEmpty()){
+               mainbinding.repassword.error = "field can't be empty!!"
                false
-           }else if(repass.text.toString() != pass.text.toString()){
-               repass.error = "mismatch!!"
+           }else if(mainbinding.repassword.text.toString() != mainbinding.password.text.toString()){
+               mainbinding.repassword.error = "mismatch!!"
                false
            }else true
         }
         fun validateGender():Boolean{
-            return if (gender.checkedRadioButtonId == -1) {
+            return if ( mainbinding.radioGroup1       .checkedRadioButtonId == -1) {
                 Toast.makeText(this, "please enter your gender", Toast.LENGTH_SHORT).show()
                 false
             }else true
         }
 
-        btnreg.setOnClickListener {
+        mainbinding.btnregister.setOnClickListener {
+
          if (!validateFullName() || !validateUserName() ||  !validateEmail() || !validatePassword() || !validateRetypePassword() || !validateGender()){
             return@setOnClickListener
          }else {
-                val sedit = shpref.edit()
-                sedit.putString("fulln", fullname.text.toString()).apply()
-                sedit.putString("usern", username.text.toString()).apply()
-                sedit.putString("passw", pass.text.toString()).apply()
-                sedit.putString("emai", email.text.toString()).apply()
-                sedit.putString(
-                    "gen", (if (gender.checkedRadioButtonId == radiobutton1.id) {
-                        radiobutton1.text
-                    } else radiobutton2.text) as String?
+
+             sharepreferences.edit().putString("fulln", mainbinding.fullname.text.toString()).apply()
+             sharepreferences.edit().putString("usern", mainbinding.username.text.toString()).apply()
+             sharepreferences.edit().putString("passw", mainbinding.password.text.toString()).apply()
+             sharepreferences.edit().putString("emai", mainbinding.email.text.toString()).apply()
+             sharepreferences.edit().putString(
+                    "gen", (if (mainbinding.radioGroup1.checkedRadioButtonId == mainbinding.radioButton1.id) {
+                        mainbinding.radioButton1.text
+                    } else mainbinding.radioButton2.text) as String?
                 ).apply()
                 Toast.makeText(this, "YOU ARE REGISTRED SUCCESSFULLY", Toast.LENGTH_SHORT).show()
-                fullname.text.clear()
-                username.text.clear()
-                email.text.clear()
-                pass.text.clear()
-                repass.text.clear()
-                gender.clearCheck()
+                mainbinding.fullname.text.clear()
+                mainbinding.username.text.clear()
+                mainbinding.email.text.clear()
+                mainbinding.password.text.clear()
+                mainbinding.repassword.text.clear()
+                mainbinding.radioGroup1.clearCheck()
             }
 
         }
-        btninf.setOnClickListener {
-            minfo.visibility = View.VISIBLE
-            fullnameinfo.text = shpref.getString("fulln", fullname.text.toString())
-            usernameinfo.text = shpref.getString("usern", username.text.toString())
-            passinfo.text = shpref.getString("passw", pass.text.toString())
-            emailinfo.text = shpref.getString("emai", email.text.toString())
-            if (gender.checkedRadioButtonId == radiobutton1.id) {
-                genderinfo.text = radiobutton1.text
+        mainbinding.btnshowinfo.setOnClickListener {
+            mainbinding.minfo.visibility = View.VISIBLE
+            mainbinding.fullnameinfo.text = sharepreferences.getString("fulln", mainbinding.fullname.text.toString())
+            mainbinding.usernameinfo.text = sharepreferences.getString("usern", mainbinding.username.text.toString())
+            mainbinding.passinfo.text = sharepreferences.getString("passw", mainbinding.passinfo.text.toString())
+            mainbinding.emailinfo.text = sharepreferences.getString("emai", mainbinding.email.text.toString())
+            if (mainbinding.radioGroup1.checkedRadioButtonId == mainbinding.radioButton1.id) {
+                mainbinding.geninfo.text = mainbinding.radioButton1.text
             } else {
-                genderinfo.text = radiobutton2.text
+                mainbinding.geninfo.text = mainbinding.radioButton1.text
             }
 
         }
         hidinfo.setOnClickListener {
-            minfo.visibility = View.INVISIBLE
+            mainbinding.minfo.visibility = View.INVISIBLE
         }
 
 
